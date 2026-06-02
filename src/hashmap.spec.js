7b160149dca12bbf15b3fa4c1a7018b86e4815a4
hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-06-02 21:44:48 +0200

    wip: write tests to test growth functionality

--- src/hashmap.spec.js
+++ src/hashmap.spec.js
@@ -109,3 +109,24 @@ describe("test entries method", () => {
     ]);
   });
 });
+describe("test growth functionality", () => {
+  const map = new HashMap();
+  beforeEach(() => {
+    map.set("apple", "red");
+    map.set("banana", "yellow");
+    map.set("carrot", "orange");
+    map.set("dog", "brown");
+    map.set("elephant", "gray");
+    map.set("frog", "green");
+    map.set("grape", "purple");
+    map.set("hat", "black");
+    map.set("ice cream", "white");
+    map.set("jacket", "blue");
+    map.set("kite", "pink");
+    map.set("lion", "golden");
+  });
+
+  test("length should be 16", () => {
+    expect(map.length()).toEqual(12);
+  });
+});
